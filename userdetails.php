838fde1dd314d84ec0e92f58359013dc30b5ab31
Aditya <aiyer786@gmail.com>
2021-07-21 16:30:24 +0530

    Autoincrement database

--- userdetails.php
+++ userdetails.php
@@ -112,7 +112,7 @@
                     }
                     else{
                         echo("<script>alert('You have Sucessfully Registred!')</script>");
-                        echo("<script>window.location = 'student.php'</script>");
+                        echo("<script>window.location = 'userdetails.php'</script>");
                     }
                     // $query = " SELECT * FROM `student` WHERE `email` = '$email' ";
                     // $res = mysqli_query($Connect, $query);
@@ -147,10 +147,10 @@
   <div class="tab-pane fade" id="pills-mentor" role="tabpanel" aria-labelledby="pills-mentor-tab">
   
   <form method="POST">
-                <div class="mb-3">
+                <!-- <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Mentor Id :</label>
                     <input type="text" class="form-control" id="exampleFormControlInput1" name="m_id">
-                </div>
+                </div> -->
                 <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Department:</label>
                         <select class="form-select" aria-label="Default select example" name="dept">
@@ -188,7 +188,6 @@
 
             <?php 
                 if(isset($_POST['mentor'])){
-                    $m_id= $_POST['m_id'];
                     $dept= $_POST['dept'];
                     $F_name=$_POST['F_name'];
                     $M_name=$_POST['M_name'];
@@ -197,7 +196,7 @@
                     $password= 'dypatil@456';
                     $contact=$_POST['contact'];
 
-                    $query = "INSERT INTO `mentor` VALUES ('$m_id','$dept','$F_name','$M_name','$L_name','$email','$password','$contact', '0')";
+                    $query ="INSERT INTO `mentor`(`dept`, `F_name`, `M_name`, `L_name`, `email`, `password`, `contact`) VALUES ('$dept','$F_name','$M_name','$L_name','$email','$password','$contact')";
                     $res = mysqli_query($Connect, $query);
                     if(!$res){
                         echo("<script>alert('Error!! data not inserted into database')</script>");
@@ -206,15 +205,18 @@
                         echo("<script>alert('You have Sucessfully Registred!')</script>");
                         echo("<script>window.location = 'userdetails.php'</script>");
                     }
-                    $query="SELECT `sr_no` FROM `mentor` WHERE `email`='$email' ";
+                    $query="SELECT * FROM `mentor` WHERE `email`='$email' ";
                     $res=mysqli_query($Connect,$query);
                     while($row=$res->fetch_assoc())
                     {
                       $sr_no="{$row['sr_no']}";
+                      $dept="{$row['dept']}";
                     }
+                    
                     $sr_no=substr(str_repeat(0,4).$sr_no,-4);
-                    $s_id="DY{$sr_no}";
-                    $query="UPDATE `student` SET `m_id` = '$m_id' WHERE `mentor`.`email` = '$email'";
+                    $m_id="DY{$dept}{$sr_no}";
+                    $query="UPDATE `mentor` SET `m_id` = '$m_id' WHERE `mentor`.`email` = '$email'";
+
                     $resu = mysqli_query($Connect, $query);
                 }
 
@@ -226,18 +228,14 @@
 
   <div class="tab-pane fade" id="pills-co_or" role="tabpanel" aria-labelledby="pills-co_or-tab">
   <form method="POST">
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Co-ordinator Id :</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1" name="c_id">
-                </div>
-                <div class="mb-3">
+  <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Department:</label>
                         <select class="form-select" aria-label="Default select example" name="dept">
                             <option selected>CS</option>
-                            <option value="1">IT</option>
-                            <option value="2">EX</option>
-                            <option value="3">ET</option>
-                            <option value="4">IN</option>
+                            <option value="IT">IT</option>
+                            <option value="EX">EX</option>
+                            <option value="ET">ET</option>
+                            <option value="IN">IN</option>
                         </select>
                     </div>
                 <div class="mb-3">
@@ -267,7 +265,6 @@
 
             <?php
             if (isset($_POST['co_or'])) {
-                $c_id = $_POST['c_id'];
                 $dept = $_POST['dept'];
                 $F_name = $_POST['F_name'];
                 $M_name = $_POST['M_name'];
@@ -276,14 +273,28 @@
                 $password = 'dypatil@456';
                 $contact = $_POST['contact'];
 
-                $query = "INSERT INTO `coordinator` VALUES ('$c_id','$dept','$F_name','$M_name','$L_name','$email','$password','$contact')";
+                $query = "INSERT INTO `coordinator`(`dept`, `F_name`, `M_name`, `L_name`, `email`, `password`, `contact`) VALUES ('$dept','$F_name','$M_name','$L_name','$email','$password','$contact')";
                 $res = mysqli_query($Connect, $query);
+
+
                 if (!$res) {
                     echo ("<script>alert('Error!! data not inserted into database')</script>");
                 } else {
                     echo ("<script>alert('You have Sucessfully Registred!')</script>");
                     echo ("<script>window.location = 'userdetails.php'</script>");
                 }
+
+                $query="SELECT * FROM `coordinator` WHERE `email`='$email' ";
+                $res = mysqli_query($Connect, $query);
+                while($row=$res->fetch_assoc())
+                    {
+                      $sr_no="{$row['sr_no']}";
+                      $dept="{$row['dept']}";
+                    }
+                    $sr_no=substr(str_repeat(0,4).$sr_no,-4);
+                    $c_id="DY{$dept}{$sr_no}";
+                    $query="UPDATE `coordinator` SET `c_id` = '$c_id' WHERE `coordinator`.`email` = '$email'";
+                    $resu = mysqli_query($Connect, $query);
             }
 
             ?>
@@ -293,17 +304,13 @@
   <div class="tab-pane fade" id="pills-hod" role="tabpanel" aria-labelledby="pills-hod-tab">
   <form method="POST">
                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">HOD Id :</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1" name="h_id">
-                </div>
-                <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Department:</label>
                         <select class="form-select" aria-label="Default select example" name="dept">
                             <option selected>CS</option>
-                            <option value="1">IT</option>
-                            <option value="2">EX</option>
-                            <option value="3">ET</option>
-                            <option value="4">IN</option>
+                            <option value="IT">IT</option>
+                            <option value="EX">EX</option>
+                            <option value="ET">ET</option>
+                            <option value="IN">IN</option>
                         </select>
                     </div>
                 <div class="mb-3">
@@ -333,7 +340,6 @@
 
             <?php
             if (isset($_POST['hod'])) {
-                $h_id = $_POST['h_id'];
                 $dept = $_POST['dept'];
                 $F_name = $_POST['F_name'];
                 $M_name = $_POST['M_name'];
@@ -342,7 +348,7 @@
                 $password = 'dypatil@456';
                 $contact = $_POST['contact'];
 
-                $query = "INSERT INTO `hod` VALUES ('$h_id','$dept','$F_name','$M_name','$L_name','$email','$password','$contact')";
+                $query = "INSERT INTO `hod`(`dept`, `F_name`, `M_name`, `L_name`, `email`, `password`, `contact`)  VALUES ('$dept','$F_name','$M_name','$L_name','$email','$password','$contact')";
                 $res = mysqli_query($Connect, $query);
                 if (!$res) {
                     echo ("<script>alert('Error!! data not inserted into database')</script>");
@@ -350,7 +356,23 @@
                     echo ("<script>alert('You have Sucessfully Registred!')</script>");
                     echo ("<script>window.location = 'userdetails.php'</script>");
                 }
+
+                $query="SELECT * FROM `hod` WHERE `email`='$email' ";
+                $res = mysqli_query($Connect, $query);
+                while($row=$res->fetch_assoc())
+                    {
+                      $sr_no="{$row['sr_no']}";
+                      $dept="{$row['dept']}";
+                    }
+                    $sr_no=substr(str_repeat(0,4).$sr_no,-4);
+                    $c_id="DY{$dept}{$sr_no}";
+                    $query="UPDATE `hod` SET `h_id` = '$c_id' WHERE `hod`.`email` = '$email'";
+                    $resu = mysqli_query($Connect, $query);
             }
+
+           
+        
+
 
             ?>
   </div>
