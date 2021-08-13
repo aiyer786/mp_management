05b37a4699b27b5d1e4bd9aa56cf7d72a1478c69
Purva Kharsambale <69680501+purva2906@users.noreply.github.com>
2021-08-13 11:34:44 +0530

    Add files via upload

---
+++ coordinator page updated/index.js
@@ -0,0 +1,24 @@
+let isGroupFormed = false;
+
+groupTab = document.getElementById("nav-group");
+
+if (isGroupFormed) {
+    groupTab.innerHTML = "Add/View Members";
+} else {
+    groupTab.innerHTML = "Create Group";
+}
+
+function getConfirmation() {
+    var retVal = confirm("Are you sure you're the group leader ?");
+    if (retVal == true) {
+        // document.write("User wants to continue!");
+        isGroupFormed = true;
+        location.reload();
+        return true;
+    } else {
+        // document.write("User does not want to continue!");
+        return false;
+    }
+}
+
+groupTab.onclick = getConfirmation;
